276821164071821fcbbf003dccd73b24f72b9f61
glaucorsg <glaucorsg@gmail.com>
2022-03-09 20:14:52 -0300

    Commit inicial

---
+++ src/main/kotlin/MainActivity.kt
@@ -0,0 +1,14 @@
+/**
+ * IFPB - TSI
+ * Padroes Dispositivos Móveis
+ * Professora Valéria Maria Bezerra Cavalcanti
+ * Aluno Renato Silva
+ * Aluno Glauco Gomes
+ * Março 2022
+ */
+
+
+
+fun main() {
+    val dic = Desafio().jogar("CASA", "É uma  habitação")
+}
